28a5abf52045a02ec9f386f7bc4cde35f6f1029a
RaulMtzF <rmartinez@inteek.mx>
2017-12-30 13:14:03 -0600

    CAMBIOS

---
+++ Framework/Libraies/ResultDeliveryType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Libraies
+{
+    public class ResultDeliveryType
+    {
+        public int IdDelivery { get; set; }
+        public Nullable<bool> DeliveryType1 { get; set; }
+        public Nullable<int> IdUser { get; set; }
+        public Nullable<int> IdWorkshop { get; set; }
+        public Nullable<int> IdServiceWorkshop { get; set; }
+        public Nullable<int> IdAppointmentsWorkshop { get; set; }
+        public Nullable<System.DateTime> Date { get; set; }
+        public string Time { get; set; }
+        public string Comments { get; set; }
+    }
+}
---
+++ Framework/Libraies/ResultProductsConfirmation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Libraies
+{
+    public class ResultProductsConfirmation
+    {
+        public string ProductName { get; set; }
+        public Nullable<int> Quantity { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<decimal> TotalPrice1 { get; set; }
+        public string ProductImage { get; set; }
+        public int IdOrderDetail { get; set; }
+    }
+}
